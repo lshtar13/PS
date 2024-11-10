4683ba40381e2e1b6b1a89d427ed4c6daba93542
lshtar13 <lhs109802@g.skku.edu>
2024-11-10 01:29:30 +0000

    .

--- prac.cc
+++ prac.cc
@@ -11,6 +11,13 @@ typedef queue<pll> qpll;
 typedef priority_queue<ll> pqll;
 typedef priority_queue<pll> pqpll;
 typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
+typedef vector<vpll> vvpll;
+#define FOR1(a, A) for (ll a = 0; a < A; ++a)
+#define FOR2(a, b, A, B)                                                       \
+  for (ll a = 0; a < A; ++a)                                                   \
+    for (ll b = 0; b < B; ++b)
 
 int main(void)
 {
@@ -18,8 +25,6 @@ int main(void)
     cin.tie(NULL);
     cout.tie(NULL);
 
-    string s = "hello";
-    cout << s * 10 << "\n";
 
     return 0;
 }
