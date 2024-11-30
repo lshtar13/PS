b8c8ac159d551dff58f9fbeae5693e0fd65525d9
lshtar13 <lhs109802@g.skku.edu>
2024-11-30 01:09:26 +0000

    .

--- 7453.cc
+++ 7453.cc
@@ -6,73 +6,61 @@ typedef unsigned long long ull;
 typedef pair<ll, ll> pll;
 typedef pair<ull, ull> pull;
 typedef const ll cll;
+typedef queue<ll> qll;
+typedef queue<pll> qpll;
+typedef priority_queue<ll> pqll;
+typedef priority_queue<pll> pqpll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
+typedef vector<vpll> vvpll;
+#define FOR1(a, A) for (ll a = 0; a < A; ++a)
+#define FOR2(a, b, A, B)                                                       \
+  for (ll a = 0; a < A; ++a)                                                   \
+    for (ll b = 0; b < B; ++b)
 
-ll n;
-pll arr[16000];
+cll N = 4000;
+ll n, A[N] = {}, B[N] = {}, C[N] = {}, D[N] = {}, AB[N * N] = {},
+      CD[N * N] = {};
 
-int main(void)
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-    cin>>n;
-    for(int num[4] = {0}, i = 0; i<n; ++i)
-    {
-        cin>>num[0]>>num[1]>>num[2]>>num[3];
-        for(int l = 0; l<4; ++l)
-        {
-            arr[4*i + l] = make_pair(num[l], l);
-        }
+  cin >> n;
+  for (ll i = 0; i < n; ++i) {
+    cin >> A[i] >> B[i] >> C[i] >> D[i];
+  }
+
+  FOR2(i, l, n, n) {
+    AB[i * n + l] = A[i] + B[l];
+    CD[i * n + l] = C[i] + D[l];
+  }
+
+  sort(AB, AB + n * n);
+  sort(CD, CD + n * n);
+
+  ll result = 0, ab = n * n - 1, cd = 0;
+  while (ab >= 0 && cd < n * n) {
+    ll sum = AB[ab] + CD[cd], nab, ncd;
+    if (sum == 0) {
+      nab = 1, ncd = 1;
+      while (ab - nab >= 0 && AB[ab] == AB[ab - nab]) {
+        ++nab;
+      }
+      while (cd + ncd < n * n && CD[cd] == CD[cd + ncd]) {
+        ++ncd;
+      }
+      result += nab * ncd, ab += nab, cd += ncd;
+    } else if (sum < 0) {
+      ++cd;
+    } else {
+      --ab;
     }
-    sort(arr, arr+4*n);
+  }
 
-    ll result = 0;
-    for(ll st0 = 0; st0<4*n; ++st0)
-    {
-        for(ll sum0, sum1, st, en, st1 = st0+1; arr[st0].second != arr[st1].second && st1<4*n; ++st1)
-        {
-            st = st1+1, en = 4*n-1, sum0 = arr[st0].first+ arr[st1].first;
+  cout << result << "\n";
 
-            while(en>st)
-            {
-                sum1 = arr[st].first + arr[en].first;
-                if(sum0 + sum1 > 0)
-                {
-                    while(en-1>st && arr[en-1] == arr[en])
-                    {
-                        --en;
-                    }
-                    --en;
-                }
-                else if(sum0 + sum1 < 0)
-                {
-                    while(st+1 < en && arr[st+1] == arr[st])
-                    {
-                        ++st;
-                    }
-                    ++st;
-                }
-                else
-                {
-
-                    ll num0 = 1, num1 = 1, check = 0;
-                    while(arr[st+1])
-                    check|=(1<<arr[st0].second);
-                    check|=(1<<arr[st1].second);
-                    check|=(1<<arr[st].second);
-                    check|=(1<<arr[en].second);
-                    if(check == 15){
-                    cout<<arr[st0].second<<" "<<arr[st1].second<<" "<<arr[st].second<<" "<<arr[en].second<<"\n";
-                        ++result;
-                    }
-                    ++st;
-                }
-            }
-        }
-    }
-
-    cout<<result<<"\n";
-
-    return 0;
+  return 0;
 }
