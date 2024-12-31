c57c75a21a002e33c7a6c06dba0fe0b2f4f4d388
lshtar13 <lhs109802@g.skku.edu>
2024-12-31 03:30:34 +0000

    .

--- 5626.cc
+++ 5626.cc
@@ -20,47 +20,13 @@ typedef vector<vpll> vvpll;
     for (ll b = 0; b < B; ++b)
 
 cll N = 1e4, INF = 1e9 + 7;
-ll n, nums[N] = {};
-int dp[N / 2 + 1][N] = {};
-
-ll find(ll prv, ll idx) {
-  ll result;
-  if (idx >= n) {
-    return 1;
-  } else if (dp[prv][idx] != -1) {
-    goto END;
-  }
-
-  if (nums[idx] != -1) {
-    if (abs(prv - nums[idx]) > 1) {
-      result = 0;
-    } else {
-      result = find(nums[idx], idx + 1);
-    }
-  } else {
-    result = 0;
-    if (prv + 1 <= n / 2) {
-      result += find(prv + 1, idx + 1);
-    }
-    result += find(prv, idx + 1);
-    if (prv >= 1) {
-      result += find(prv - 1, idx + 1);
-    }
-  }
-
-  result %= INF;
-  dp[prv][idx] = result;
-
-END:
-  return dp[prv][idx];
-}
+ll n, nums[N] = {}, dp[2][N] = {{}};
 
 int main(void) {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  memset(dp, -1, sizeof(dp));
   cin >> n;
   for (ll i = 0; i < n; ++i) {
     cin >> nums[i];
@@ -70,8 +36,26 @@ int main(void) {
   } else if (nums[n - 1] != -1 && nums[n - 1] != 0) {
     cout << "0\n";
   } else {
-    nums[0] = nums[n - 1] = 0;
-    cout << find(0, 1) << "\n";
+    nums[0] = nums[n - 1] = 0, dp[n % 2][0] = 1;
+    for (ll idx = n - 1; idx >= 0; --idx) {
+      for (ll num = 0; num <= n / 2; ++num) {
+        if (nums[idx] != -1 && nums[idx] != num) {
+          dp[idx % 2][num] = 0;
+          continue;
+        }
+
+        dp[idx % 2][num] = dp[(idx + 1) % 2][num];
+        if (num >= 1) {
+          dp[idx % 2][num] += dp[(idx + 1) % 2][num - 1];
+        }
+        if (num < n / 2) {
+          dp[idx % 2][num] += dp[(idx + 1) % 2][num + 1];
+        }
+        dp[idx % 2][num] %= INF;
+      }
+    }
+
+    cout << dp[0][0] << "\n";
   }
 
   return 0;
