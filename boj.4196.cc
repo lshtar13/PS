6337fad55198c530d4a2612463f2c0e1470768f1
lshtar13 <lhs109802@g.skku.edu>
2025-05-15 10:35:05 +0000

    .

--- boj.4196.cc
+++ boj.4196.cc
@@ -20,35 +20,81 @@ typedef vector<vpll> vvpll;
     for (ll b = 0; b < B; ++b)
 
 cll N = 1e5, M = 1e5;
-ll n, m;
+ll n, m, parents[N], groups[N] = {}, gidx, pidx;
+stack<ll> prvs;
 
-ll solve() {
-  cin >> n >> m;
-
-  vll parents(n, -1);
-  vvll edges(n);
-  vector<bool> finished(n, false);
-
-  for (ll i = 0, a, b; i < m; ++i) {
-    cin >> a >> b;
-    --a, --b;
-    edges[a].push_back(b);
+ll dfs(ll node, vvll &edges) {
+  if (parents[node] != -1) {
+    return parents[node];
   }
 
-  stack<ll> s;
-  for (ll i = 0; i < n; ++i) {
-    if (finished[i]) {
+  ll parent = parents[node] = pidx++;
+  prvs.push(node);
+  for (auto &av : edges[node]) {
+    if (groups[av] != -1) {
       continue;
     }
 
-    s.push(i);
-    parents[i] = i;
-    for(auto &av: edges[s.top()]){
-        if(parents[av] !=av)
+    parents[node] = min(parents[node], dfs(av, edges));
+  }
+
+  for (ll prv; parents[node] == parent && !prvs.empty();) {
+    prv = prvs.top();
+    prvs.pop();
+
+    groups[prv] = gidx;
+
+    if (prv == node) {
+      ++gidx;
+      break;
+    }
+  }
+
+  return parents[node];
+}
+
+ll solve() {
+  cin >> n >> m;
+  vvll edges(n);
+  for (ll i = 0, x, y; i < m; ++i) {
+    cin >> x >> y;
+    edges[--x].emplace_back(--y);
+  }
+
+  gidx = 0, pidx = 0;
+  memset(parents, -1, sizeof(parents));
+  memset(groups, -1, sizeof(groups));
+  for (ll i = 0; i < n; ++i) {
+    if (groups[i] != -1) {
+      continue;
+    }
+
+    dfs(i, edges);
+  }
+
+  set<pll> checked;
+  vll degrees(gidx, 0);
+  for (ll node = 0; node < n; ++node) {
+    for (auto &av : edges[node]) {
+      ll a = groups[node], b = groups[av];
+
+      if (a == b) {
+        continue;
+      }
+
+      if (checked.find({a, b}) == checked.end()) {
+        checked.insert({a, b});
+        ++degrees[b];
+      }
     }
   }
 
   ll result = 0;
+  for (ll i = 0; i < gidx; ++i) {
+    if (!degrees[i]) {
+      ++result;
+    }
+  }
 
   return result;
 }
