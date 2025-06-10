706856e56ec96b5b56beaad54ff12f3f4bb7ea0b
lshtar13 <lhs109802@g.skku.edu>
2025-06-10 11:29:26 +0000

    .

--- boj.1981.cc
+++ boj.1981.cc
@@ -11,87 +11,86 @@ typedef queue<pll> qpll;
 typedef priority_queue<ll> pqll;
 typedef priority_queue<pll> pqpll;
 typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
+typedef vector<vpll> vvpll;
+#define FOR(a, A) for (ll a = 0; a < A; ++a)
 
-cll MAX_N = 100;
-ll n, mat[MAX_N][MAX_N] = {{}}, directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+cll N = 100, Num = 200, directions[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+ll n, mat[N][N] = {{}};
 
-inline bool isValidCord(ll i, ll l)
-{
-    return i >= 0 && i < n && l >= 0 && l < n;
+inline bool isValidCord(ll i, ll l) {
+  return i >= 0 && i < n && l >= 0 && l < n;
 }
 
-bool isValid(ll gap)
-{
-    vector<vll> maxV(n, vll(n, 201)), minV(n, vll(n, -1));
-    qll q;
-    q.push(0);
-    maxV[0][0] = mat[0][0], minV[0][0] = mat[0][0];
-    while (!q.empty())
-    {
-        ll i = q.front() / n, l = q.front() % n;
-        q.pop();
-        if (i == n - 1 && l == n - 1)
-        {
-            return true;
-        }
-
-        for (auto &d : directions)
-        {
-            ll _i = i + d[0], _l = l + d[1];
-            if (!isValidCord(_i, _l))
-            {
-                continue;
-            }
-
-            ll _maxV = max(maxV[i][l], mat[_i][_l]), _minV = min(minV[i][l], mat[_i][_l]);
-            if ((_maxV < maxV[_i][_l] || _minV > minV[_i][_l]) && (_maxV - _minV <= gap))
-            {
-                maxV[_i][_l] = _maxV, minV[_i][_l] = _minV;
-                q.push(_i * n + _l);
-            }
-        }
-    }
-
+bool avail(ll low, ll high) {
+  bool visited[N][N] = {{}};
+  if (mat[0][0] < low || mat[0][0] > high) {
     return false;
+  }
+
+  qpll q;
+  q.push({0, 0});
+  visited[0][0] = true;
+  while (!q.empty()) {
+    ll i = q.front().first, l = q.front().second;
+    q.pop();
+
+    for (auto &d : directions) {
+      ll ni = i + d[0], nl = l + d[1];
+      if (!isValidCord(ni, nl) || visited[ni][nl] || mat[ni][nl] < low ||
+          mat[ni][nl] > high) {
+        continue;
+      }
+
+      visited[ni][nl] = true;
+      q.push({ni, nl});
+    }
+  }
+
+  return visited[n - 1][n - 1];
 }
 
-int main(void)
-{
-    // auto start = chrono::high_resolution_clock::now();
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-    ll mx = 0, mn = 200;
-    cin >> n;
-    for (ll i = 0; i < n; ++i)
-    {
-        for (ll l = 0; l < n; ++l)
-        {
-            cin >> mat[i][l];
-            st = min(mx, mat[i][l]), en = max(mn, mat[i][l]);
-        }
+  cin >> n;
+  FOR(i, n) FOR(l, n) { cin >> mat[i][l]; }
+
+  //  ll result = Num;
+  //  for (ll low = 0, high; low <= min(mat[0][0], mat[n - 1][n - 1]); ++low) {
+  //    ll st = low, en = Num, ans = -1;
+  //    while (st <= en) {
+  //      ll mid = (st + en) / 2;
+  //      if (avail(low, mid)) {
+  //        en = mid - 1, ans = mid;
+  //      } else {
+  //        st = mid + 1;
+  //      }
+  //    }
+  //    if (ans != -1) {
+  //      result = min(result, ans - low);
+  //    }
+  //  }
+
+  //  cout << result << "\n";
+  ll result = Num, st = 0, en = 0;
+  while (en <= Num) {
+    if (avail(st, en)) {
+      result = min(result, en - st);
+      ++st;
+    } else {
+      ++en;
     }
 
-    ll st = mx - mn, en = 0, mid, ans;
-    while (st <= en)
-    {
-        // cout << st << " " << en << '\n';
-        mid = (st + en) / 2;
-        if (isValid(mid))
-        {
-            ans = mid, en = mid - 1;
-        }
-        else
-        {
-            st = mid + 1;
-        }
+    while (en < st) {
+      ++en;
     }
+  }
 
-    cout << ans << "\n";
+  cout << result << "\n";
 
-    // auto end = chrono::high_resolution_clock::now();
-    // auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
-    // cout << "실행 시간: " << duration.count() << "ms" << "\n";
-    return 0;
+  return 0;
 }
